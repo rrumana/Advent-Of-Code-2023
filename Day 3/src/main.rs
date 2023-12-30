ce003824953d3d6b64962a4a4b5597ba5ee92fd3
rrumana <ryan.rumana@gmail.com>
2023-12-30 12:24:28 -0800

    cleaned up code

--- Day 3/src/main.rs
+++ Day 3/src/main.rs
@@ -8,34 +8,59 @@ struct PartNumber {
 } impl PartNumber {
     fn new(row: i64, col: i64, ch: char) -> Self {
         let points = HashSet::from([
-            (row - 1, col - 1),
-            (row, col - 1),
-            (row + 1, col - 1), // left hand side
-            (row - 1, col),
-            (row + 1, col), // above and below
-            (row - 1, col + 1),
-            (row, col + 1),
-            (row + 1, col + 1), // right hand side
+            (row - 1, col - 1), (row, col - 1), (row + 1, col - 1), // left hand side
+            (row - 1, col), (row + 1, col),                         // above and below
+            (row - 1, col + 1), (row, col + 1), (row + 1, col + 1), // right hand side
         ]);
         Self {
-            value: (ch as u8 - b'0') as i64,
+            value: ch.to_digit(10).unwrap() as i64,
             points,
         }
     }
 
-    fn add_digit(&mut self, row: i64, col: i64, ch: char) {
-        self.value = self.value * 10 + (ch as u8 - b'0') as i64;
-        self.points
-            .extend([(row - 1, col + 1), (row, col + 1), (row + 1, col + 1)]);
+    fn extend(&mut self, row: i64, col: i64, ch: char) {
+        self.value = self.value * 10 + ch.to_digit(10).unwrap() as i64;
+        self.points.extend([(row - 1, col + 1), (row, col + 1), (row + 1, col + 1)]);
     }
 
-    fn extract_value(&self) -> i64 {
+    fn get_value(&self) -> i64 {
         self.value
     }
 
-    fn next_to_symbol(&self, syms: &HashSet<(i64, i64)>) -> bool {
+    fn by_symbol(&self, syms: &HashSet<(i64, i64)>) -> bool {
         self.points.intersection(&syms).next().is_some()
     }
+}
+
+fn part_one(nums: &Vec<PartNumber>, syms: &HashSet<(i64, i64)>) -> i64 {
+    let total = nums
+        .iter()
+        .filter(|num| num.by_symbol(&syms))
+        .map(PartNumber::get_value) 
+        .sum::<i64>();
+
+    total
+}
+
+fn part_two(nums: &Vec<PartNumber>, gears: &HashSet<(i64, i64)>) -> i64 {
+    let mut total = 0;
+
+    'next_gear: for gear in gears {
+        let mut matches = Vec::new();
+        for num in nums {
+            if num.points.contains(&gear) {
+                if matches.len() == 2 {
+                    continue 'next_gear;
+                }
+                matches.push(num.value);
+            }
+        }
+        if matches.len() == 2 {
+            total += matches[0] * matches[1];
+        }
+    }
+
+    total
 }
 
 fn main() {
@@ -48,7 +73,7 @@ fn main() {
         .collect();
 
     let mut nums: Vec<PartNumber> = Vec::new();
-    let mut symbols: HashSet<(i64, i64)> = HashSet::new();
+    let mut syms: HashSet<(i64, i64)> = HashSet::new();
     let mut gears: HashSet<(i64, i64)> = HashSet::new();
 
     let mut last: Option<PartNumber> = None;
@@ -58,7 +83,7 @@ fn main() {
         for (col, ch) in extended.chars().enumerate() {
             if ch.is_ascii_digit() {
                 if let Some(ref mut num) = last {
-                    num.add_digit(row as i64, col as i64, ch);
+                    num.extend(row as i64, col as i64, ch);
                 } else {
                     last = Some(PartNumber::new(row as i64, col as i64, ch));
                 }
@@ -67,7 +92,7 @@ fn main() {
                     nums.push(num);
                 }
                 if ch != '.' && ch != '\r' {
-                    symbols.insert((row as i64, col as i64));
+                    syms.insert((row as i64, col as i64));
                     if ch == '*' {
                         gears.insert((row as i64, col as i64));
                     }
@@ -76,30 +101,11 @@ fn main() {
         }
     }
 
-    //let _total_one = nums
-    //    .iter()
-    //    .filter(|num| num.next_to_symbol(&symbols))
-    //    .map(PartNumber::extract_value) 
-    //    .sum::<i64>();
+    let total_one: i64 = part_one(&nums, &syms);
+    let total_two: i64 = part_two(&nums, &gears);
 
-    let mut total_two = 0;
-
-    'next_gear: for gear in gears {
-        let mut matches = Vec::new();
-        for num in &nums {
-            if num.points.contains(&gear) {
-                if matches.len() == 2 {
-                    continue 'next_gear;
-                }
-                matches.push(num.value);
-            }
-        }
-        if matches.len() == 2 {
-            total_two += matches[0] * matches[1];
-        }
-    }
-
-    println!("{}", total_two);
+    println!("Part one answer: {}", total_one);
+    println!("Part two answer: {}", total_two);
 }
 
 #[cfg(test)]
