bbd4d2b0bd24bdfac075a352eb8a0d3c40bf10c9
rrumana <ryan.rumana@gmail.com>
2024-01-01 12:26:14 -0800

    added part one implementation

--- Day 4/src/main.rs
+++ Day 4/src/main.rs
@@ -1,7 +1,36 @@
 use std::fs::read_to_string;
+use std::collections::HashSet;
+use std::convert::TryInto;
 
-fn part_one() -> i64 {
-    return 0;
+#[derive(Debug)]
+struct Card {
+    card: i64,
+    winning: HashSet<i64>,
+    numbers: HashSet<i64>
+} impl Card {
+    fn new(card: i64, winning: HashSet<i64>, numbers: HashSet<i64>,) -> Self {
+        Self {
+            card: card,
+            winning: winning,
+            numbers: numbers
+        }
+    }
+
+    fn get_score(&self) -> i64 {
+        let count = self
+            .winning
+            .intersection(&self.numbers)
+            .count();
+        if count > 0 {
+            1 << (count -1)
+        } else {
+            0
+        }
+    }
+}
+
+fn part_one(cards: &Vec<Card>) -> i64 {
+    cards.iter().map(Card::get_score).sum::<i64>()
 }
 
 fn part_two() -> i64 {
@@ -10,14 +39,33 @@ fn part_two() -> i64 {
 
 fn main() {
     let filepath = "real_input.txt";
-    let _lines: Vec<String> = read_to_string(filepath)
+    let lines: Vec<String> = read_to_string(filepath)
         .expect("Something went wrong reading the file")
         .split('\n')
         .filter(|s| !s.is_empty())
         .map(|s| s.to_string())
         .collect();
 
-    let total_one: i64 = part_one();
+    let mut cards: Vec<Card> = Vec::new();
+
+    for line in lines.iter() {
+        let (_, nums) = line.split_once(": ").unwrap();
+        let (winning, chosen) = nums.split_once(" | ").unwrap();
+
+        let winning_nums = winning
+            .split_whitespace()
+            .map(|snum| snum.parse::<i64>().unwrap())
+            .collect::<HashSet<_>>();
+
+        let chosen_nums = chosen
+            .split_whitespace()
+            .map(|snum| snum.parse::<i64>().unwrap())
+            .collect::<HashSet<_>>();
+
+        cards.push(Card::new((cards.len()+1).try_into().unwrap(), winning_nums, chosen_nums));
+    }
+
+    let total_one: i64 = part_one(&cards);
     let total_two: i64 = part_two();
 
     println!("Part one answer: {}", total_one);
